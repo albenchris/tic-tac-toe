20df9dc48ed5b72c1beacb1bb13e23e4386f857e
albenchris <albenchris00@gmail.com>
2021-08-04 13:09:48 -0500

    refactored server for scalability

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
-const io = require('socket.io')(3001, {
+const app = require('express')();
+const PORT = process.env.PORT || 3001;
+
+const server = app.listen(
+    PORT,
+    console.log(`Server is running on port: ${PORT}`)
+);
+
+const io = require('socket.io')(server, {
     cors: {
         origin: ['http://localhost:3000']
     }
 });
+
+const { join_User, get_Current_User, user_Disconnect } = require('./dummyuser');
+
+
+// const io = require('socket.io')(3001, {
+//     cors: {
+//         origin: ['http://localhost:3000']
+//     }
+// });
 
 io.on('connection', socket => {
     console.log(socket.id);
